bdf44ff1cad7ffc70ef8f2a8d5f8130032eba56e
leiqiaochu <875386244@qq.com>
2025-01-23 10:41:03 +0800

    fix: bug#33871【资源库】当IOT端未链接到资源库时，资源详情中应不展示“检查更新”字段

--- views/resource/Resource/Detail/Update/index.vue
+++ views/resource/Resource/Detail/Update/index.vue
@@ -1,106 +1,138 @@
 <template>
-    <a-modal
-        title="检查更新"
-        visible
-        @cancel="emits('close')"
-        :maskClosable="false"
-        :width="600"
-        :footer="null"
-        @ok="emits('close')"
-    >
+  <a-modal
+      title="检查更新"
+      visible
+      @cancel="emits('close')"
+      :maskClosable="false"
+      :width="600"
+      :footer="null"
+      @ok="emits('close')"
+  >
     <div class="ht_40">
-        <div v-if="showUpdate" class="content">
+      <div v-if="loading" class="spin">
+        <a-spin ></a-spin>
+      </div>
+      <div v-else>
+        <div v-if="errorMessage" class="error">
+          <AIcon style="font-size: 100px;margin: 24px 0" type="GlobalOutlined"></AIcon>
+          <div >{{ errorMessage }}</div>
+        </div>
+        <template v-else>
+          <div v-if="showUpdate" class="content">
             <div class="title">
-                <div><a-badge status="success" />检测到有新版本</div>
-                <a-button type="primary" @click="onUpdate">更新</a-button>
+              <div>
+                <a-badge status="success"/>
+                检测到有新版本
+              </div>
+              <a-button type="primary" @click="onUpdate">更新</a-button>
             </div>
             <div class="log">
-                <h3>新版本 {{ info.version }}</h3>
-                <p>{{ info.describe }}</p>
+              <h3>新版本 {{ info.version }}</h3>
+              <p>{{ info.describe }}</p>
             </div>
-        </div>
-        <div class="noUpdate" v-else><a-badge status="success" />当前已是最新版本</div>
+          </div>
+          <div class="noUpdate" v-else>
+            <a-badge status="success"/>
+            当前已是最新版本
+          </div>
+        </template>
+      </div>
     </div>
-      <Install v-if="showUpdateModal"  @close="closeUpdateModal"/>
-    </a-modal>
+    <Install v-if="showUpdateModal" @close="closeUpdateModal"/>
+  </a-modal>
 </template>
 
 <script setup name="Update">
-import { checkUpdate,_latest,installResource } from '@device/api/resource/resource';
-import { onlyMessage } from '@jetlinks-web/utils';
+import {checkUpdate, _latest, installResource} from '@device/api/resource/resource';
+import {onlyMessage} from '@jetlinks-web/utils';
 import Install from '@device/views/resource/Resource/Install/index.vue'
-import { provide } from "vue";
+import {provide} from "vue";
 
-const emits = defineEmits(['close','refresh']);
+const emits = defineEmits(['close', 'refresh']);
 
 const props = defineProps({
-    data: {
-        type: Object,
-        default: () => ({}),
-    },
+  data: {
+    type: Object,
+    default: () => ({}),
+  },
 });
 
 const showUpdate = ref(false);
-const showUpdateModal = ref(false)
+const showUpdateModal = ref(false);
+const loading = ref(true)
+const errorMessage = ref('')
 const info = ref({});
 const getUpdate = async () => {
-    const res = await checkUpdate(props.data.id);
-    if(res.success){
-        showUpdate.value = res.result;
-        if(res.result){
-            const resp = await _latest(props.data.id);
-            if(resp.success){
-                info.value = resp.result
-            }
-        }
+  const res = await checkUpdate(props.data.id).catch(e => {
+    errorMessage.value = e?.response?.data?.message
+    loading.value = false
+  });
+  if (res.success) {
+    loading.value = false
+    showUpdate.value = res.result;
+    if (res.result) {
+      const resp = await _latest(props.data.id);
+      if (resp.success) {
+        info.value = resp.result
+      }
     }
+  }
 };
 
-const onUpdate = async() => {
-    const res = await installResource({
-        type: 'cloud',
-        resourceDetails:[{
-            releaseDetail: info.value
-        }]
-    });
-    if(res.success){
-        showUpdateModal.value = true;
-    }
+const onUpdate = async () => {
+  const res = await installResource({
+    type: 'cloud',
+    resourceDetails: [{
+      releaseDetail: info.value
+    }]
+  });
+  if (res.success) {
+    showUpdateModal.value = true;
+  }
 };
 
-const closeUpdateModal = () =>{
+const closeUpdateModal = () => {
   showUpdateModal.value = false;
   emits('refresh')
 }
 
-provide('closeUpdateModal',closeUpdateModal)
+provide('closeUpdateModal', closeUpdateModal)
 
 onMounted(() => {
-    getUpdate();
+  getUpdate();
 });
 
 </script>
 
 <style lang="less" scoped>
-.ht_40{
-    min-height: 40vh;
+.ht_40 {
+  min-height: 40vh;
+}
+.spin{
+  text-align: center;
 }
 .content {
-    background-color: #eee;
-    padding: 12px;
-    border-radius: 6px;
-    .title {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
-    .log {
-        margin-top: 12px;
-    }
+  background-color: #eee;
+  padding: 12px;
+  border-radius: 6px;
+
+  .title {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .log {
+    margin-top: 12px;
+  }
 }
-.noUpdate{
-    background-color: #eee;
-    padding: 12px;
-    border-radius: 6px;
+
+.noUpdate {
+  background-color: #eee;
+  padding: 12px;
+  border-radius: 6px;
+}
+.error{
+  text-align: center;
 }
 </style>
