2a4e6d58137654187e95bb622b1a116e5b26c904
FuHao <62583365+haodd0216@users.noreply.github.com>
2025-02-19 18:28:25 +0800

    fix: (资源库)修复更新资源弹窗选择处理方式错误的问题
    * fix: (资源库)修复更新资源弹窗选择处理方式错误的问题

--- views/resource/Resource/Detail/Apply/CardItem.vue
+++ views/resource/Resource/Detail/Apply/CardItem.vue
@@ -58,10 +58,6 @@ const isExpand = ref(true);
 const _data = ref();
 
 const onSelect = (item) => {
-    if(_selectKey.value === item.value) {
-      _selectKey.value = '';
-      return;
-    }
     _selectKey.value = item.value;
 
     if (props.type === 'metadata') {
