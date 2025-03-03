c43539a9fd2ce2cf28a4811a3cde594da1dc2612
sunchao <2642441182@qq.com>
2025-03-03 13:55:43 +0800

    fix: 34547

--- views/device/Instance/Detail/Parsing/index.vue
+++ views/device/Instance/Detail/Parsing/index.vue
@@ -1,57 +1,58 @@
 
 <template>
-    <div>
+    <div class="parsing">
+      <div class="parsing-top">
         <div class="top">
-            <div class="top-left">
-                <div>
-                    <AIcon type="ExclamationCircleOutlined" />
-                    <template v-if="topTitle === 'rest'">
-                        {{ $t('Parsing.index.217769-0') }}
-                        <j-permission-button
-                            type="link"
-                            hasPermission="device/Instance:update"
-                            @click="rest()"
-                        >
-                            {{ $t('Parsing.index.217769-1') }}
-                        </j-permission-button>
-                        {{ $t('Parsing.index.217769-2') }}
-                    </template>
-                    <template v-else>
-                        {{ $t('Parsing.index.217769-3') }}
-                        <j-permission-button
-                            type="link"
-                            hasPermission="device/Instance:update"
-                            @click="readOnly = false"
-                            :style="color"
-                        >
-                            {{ $t('Parsing.index.217769-4') }}
-                        </j-permission-button>
-                        {{ $t('Parsing.index.217769-5') }}
-                    </template>
-                </div>
-            </div>
+          <div class="top-left">
             <div>
-                {{ $t('Parsing.index.217769-6') }}
-                <a-select
-                    :defaultValue="'JavaScript'"
-                    style="width: 200px; margin-left: 5px"
+              <AIcon type="ExclamationCircleOutlined" />
+              <template v-if="topTitle === 'rest'">
+                {{ $t('Parsing.index.217769-0') }}
+                <j-permission-button
+                    type="link"
+                    hasPermission="device/Instance:update"
+                    @click="rest()"
                 >
-                    <a-select-option value="JavaScript"
-                        >JavaScript(ECMAScript 5)</a-select-option
-                    >
-                </a-select>
-                <AIcon
-                    type="ExpandOutlined"
-                    style="margin-left: 20px"
-                    @click="toggle"
-                />
+                  {{ $t('Parsing.index.217769-1') }}
+                </j-permission-button>
+                {{ $t('Parsing.index.217769-2') }}
+              </template>
+              <template v-else>
+                {{ $t('Parsing.index.217769-3') }}
+                <j-permission-button
+                    type="link"
+                    hasPermission="device/Instance:update"
+                    @click="readOnly = false"
+                    :style="color"
+                >
+                  {{ $t('Parsing.index.217769-4') }}
+                </j-permission-button>
+                {{ $t('Parsing.index.217769-5') }}
+              </template>
             </div>
+          </div>
+          <div>
+            {{ $t('Parsing.index.217769-6') }}
+            <a-select
+                :defaultValue="'JavaScript'"
+                style="width: 200px; margin-left: 5px"
+            >
+              <a-select-option value="JavaScript"
+              >JavaScript(ECMAScript 5)</a-select-option
+              >
+            </a-select>
+            <AIcon
+                type="ExpandOutlined"
+                style="margin-left: 20px"
+                @click="toggle"
+            />
+          </div>
         </div>
         <div class="edit" ref="el">
-            <div
-                v-show="readOnly"
-                class="edit-only"
-                @click="
+          <div
+              v-show="readOnly"
+              class="edit-only"
+              @click="
                     () => {
                         message.warning({
                             key: 1,
@@ -62,64 +63,64 @@
                         });
                     }
                 "
-            ></div>
-            <j-monaco-editor
-                language="javascript"
-                style="height: 100%"
-                theme="vs"
-                v-model:modelValue="editorValue"
-                :registrationTypescript="typescriptTip"
-                :init="editorInit"
-            />
+          ></div>
+          <j-monaco-editor
+              language="javascript"
+              style="height: 100%"
+              theme="vs"
+              v-model:modelValue="editorValue"
+              :registrationTypescript="typescriptTip"
+              :init="editorInit"
+          />
         </div>
         <div class="bottom">
-            <div style="width: 49.5%">
-                <div class="bottom-title">
-                    <div class="bottom-title-text">{{ $t('Parsing.index.217769-8') }}</div>
-                    <div class="bottom-title-topic">
-                        <template
-                            v-if="instanceStore.current.transport === 'MQTT'"
-                        >
-                            <div style="margin-right: 5px">Topic:</div>
-                            <a-auto-complete
-                                :placeholder="$t('Parsing.index.217769-9')"
-                                style="width: 300px"
-                                :options="topicList"
-                                :allowClear="true"
-                                :filterOption="filterOption"
-                                v-model:value="topic"
-                            />
-                        </template>
-                        <template v-else>
-                            <div style="margin-right: 5px">URL:</div>
-                            <a-input
-                                :placeholder="$t('Parsing.index.217769-10')"
-                                v-model:value="url"
-                                style="width: 300px"
-                            ></a-input>
-                        </template>
-                    </div>
-                </div>
-                <a-textarea
-                    :rows="5"
-                    placeholder="// 二进制数据以0x开头的十六进制输入，字符串数据输入原始字符串"
-                    style="margin-top: 10px"
-                    v-model:value="simulation"
-                />
+          <div style="width: 49.5%">
+            <div class="bottom-title">
+              <div class="bottom-title-text">{{ $t('Parsing.index.217769-8') }}</div>
+              <div class="bottom-title-topic">
+                <template
+                    v-if="instanceStore.current.transport === 'MQTT'"
+                >
+                  <div style="margin-right: 5px">Topic:</div>
+                  <a-auto-complete
+                      :placeholder="$t('Parsing.index.217769-9')"
+                      style="width: 300px"
+                      :options="topicList"
+                      :allowClear="true"
+                      :filterOption="filterOption"
+                      v-model:value="topic"
+                  />
+                </template>
+                <template v-else>
+                  <div style="margin-right: 5px">URL:</div>
+                  <a-input
+                      :placeholder="$t('Parsing.index.217769-10')"
+                      v-model:value="url"
+                      style="width: 300px"
+                  ></a-input>
+                </template>
+              </div>
             </div>
-            <div style="width: 49.5%">
-                <div class="bottom-title">
-                    <div class="bottom-title-text">{{ $t('Parsing.index.217769-11') }}</div>
-                </div>
-                <a-textarea
-                    :autoSize="{ minRows: 5 }"
-                    :style="resStyle"
-                    v-model:value="result"
-                />
+            <a-textarea
+                :rows="5"
+                placeholder="// 二进制数据以0x开头的十六进制输入，字符串数据输入原始字符串"
+                style="margin-top: 10px"
+                v-model:value="simulation"
+            />
+          </div>
+          <div style="width: 49.5%">
+            <div class="bottom-title">
+              <div class="bottom-title-text">{{ $t('Parsing.index.217769-11') }}</div>
             </div>
+            <a-textarea
+                :autoSize="{ minRows: 5 }"
+                :style="resStyle"
+                v-model:value="result"
+            />
+          </div>
         </div>
-    </div>
-    <div style="margin-top: 10px; margin-left: 10px">
+      </div>
+      <div style="padding-top: 10px">
         <j-permission-button
             type="primary"
             hasPermission="device/Instance:update"
@@ -130,7 +131,7 @@
                 title: $t('Parsing.index.217769-12'),
             }"
         >
-            {{ $t('Parsing.index.217769-13') }}
+          {{ $t('Parsing.index.217769-13') }}
         </j-permission-button>
         <j-permission-button
             hasPermission="device/Instance:update"
@@ -142,8 +143,9 @@
                 title: isTest ? '' : $t('Parsing.index.217769-14'),
             }"
         >
-            {{ $t('Parsing.index.217769-15') }}
+          {{ $t('Parsing.index.217769-15') }}
         </j-permission-button>
+      </div>
     </div>
 </template>
 
@@ -211,7 +213,7 @@ const resStyle = computed(() =>
 const filterOption = (inputValue: any, option: any) =>
     option!.value.indexOf(inputValue) !== -1;
 
-const isDisabled = computed(() => simulation.value === '');
+const isDisabled = computed(() => simulation.value === '' || readOnly.value);
 
 const result = computed(() =>
     resultValue.value.success
@@ -387,51 +389,65 @@ watch(() => instanceStore.current?.id, () => {
 </script>
 
 <style scoped lang='less'>
-.top {
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 10px;
+.parsing {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 
-    .top-left {
+  .parsing-top {
+    flex: 1;
+    min-height: 0;
+    display: flex;
+    flex-direction: column;
+
+    .top {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 10px;
+
+      .top-left {
         display: flex;
         align-items: center;
+      }
     }
-}
 
-.edit {
-    height: 550px;
-    border: 1px solid #dcdcdc;
+    .edit {
+      flex: 1;
+      min-height: 0;
+      //height: 550px;
+      border: 1px solid #dcdcdc;
 
-    .edit-only {
-        height: 550px;
-        width: 97%;
+      .edit-only {
+        //height: 550px;
         position: absolute;
         z-index: 1;
         background-color: #eeeeee70;
         cursor: not-allowed;
+      }
     }
-}
 
-.bottom {
-    display: flex;
-    justify-content: space-between;
-    padding: 10px;
-    background-color: #f7f7f7;
+    .bottom {
+      display: flex;
+      justify-content: space-between;
+      padding: 10px;
+      background-color: #f7f7f7;
 
-    .bottom-title {
+      .bottom-title {
         display: flex;
         justify-content: space-between;
 
         .bottom-title-text {
-            font-weight: 600;
-            font-size: 14px;
-            margin-top: 10px;
+          font-weight: 600;
+          font-size: 14px;
+          margin-top: 10px;
         }
 
         .bottom-title-topic {
-            display: flex;
-            align-items: center;
+          display: flex;
+          align-items: center;
         }
+      }
     }
+  }
 }
 </style>
