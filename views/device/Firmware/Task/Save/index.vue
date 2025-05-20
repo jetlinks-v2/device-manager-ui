9f3778a44bbfb15792f3c26788d2b9a95c4f9917
sunchao <2642441182@qq.com>
2025-05-20 11:10:46 +0800

    fix: 远程升级

--- views/device/Firmware/Task/Save/index.vue
+++ views/device/Firmware/Task/Save/index.vue
@@ -112,7 +112,7 @@
 <script lang="ts" setup name="TaskPage">
 import { queryProduct, saveTask } from '../../../../../api/firmware';
 import type { FormInstance } from 'ant-design-vue';
-import SelectDevices from './SelectDevices/index.vue';
+import SelectDevices from './SelectDevices.vue';
 import { useI18n } from 'vue-i18n';
 
 const { t: $t } = useI18n();
