84dcf922a58426a4f3994806bd4e4322396b8d57
sunchao <2642441182@qq.com>
2025-05-20 09:55:40 +0800

    fix: 修改里程碑2的物模型相关功能

--- components/Metadata/components/File/index.vue
+++ components/Metadata/components/File/index.vue
@@ -10,28 +10,15 @@
         <a-form ref="formRef" :model="formData" layout="vertical">
           <Type v-model:value="formData.bodyType" name="bodyType"/>
           <a-form-item
-              name="fileType"
-              :rules="[{ required: true, message: $t('File.Type.998289-3') }]"
+              name="mediaType"
+              :rules="[{ required: true, message: $t('File.Type.998289-1') }]"
+              :label="$t('File.Type.998289-0')"
           >
-            <template #label>
-              <span>
-                        {{$t('File.Type.998289-2')}}
-                        <a-tooltip :title="$t('File.Type.998289-4')">
-                            <AIcon
-                                type="QuestionCircleOutlined"
-                                style="margin-left: 2px"
-                            />
-                        </a-tooltip>
-                    </span>
-            </template>
-            <a-auto-complete
-                v-model:value="formData.fileType"
+            <JTreeSelect
+                v-model:value="formData.mediaType"
                 :options="typeOptions"
-                :dropdownStyle="{zIndex: 1999}"
-                :getPopupContainer="(node) => tableWrapperRef || node"
-            >
-              <a-input :placeholder="$t('File.Type.998289-3')" maxLength="64"/>
-            </a-auto-complete>
+                :placeholder="$t('File.Type.998289-1')"
+            />
           </a-form-item>
         </a-form>
       </div>
@@ -50,13 +37,14 @@
 import Type from './Type.vue'
 import {PopoverModal} from '../index'
 import {Form} from "ant-design-vue";
-import {useTableWrapper} from "@device/components/Metadata/context";
+import {getFileType} from "@/modules/device-manager-ui/api/product";
+import JTreeSelect from './JTreeSelect.vue';
 
 const emit = defineEmits(['update:value', 'confirm', 'cancel']);
 const props = defineProps({
   value: {
     type: Object,
-    default: () => ({bodyType: undefined, fileType: undefined}),
+    default: () => ({bodyType: undefined, mediaType: undefined}),
   },
   placement: {
     type: String,
@@ -69,112 +57,15 @@ const props = defineProps({
 });
 
 const formItemContext = Form.useInjectFormItemContext();
-const tableWrapperRef = useTableWrapper()
 const formRef = ref();
 const formData = reactive({
   bodyType: undefined,
-  fileType: undefined
+  mediaType: undefined
 });
 
 const visible = ref(false)
 
-const typeOptions = [
-  {
-    "value": "文档",
-    "label": "文档",
-    "options": [
-      {"label": ".pdf", "value": ".pdf"},
-      {"label": ".doc", "value": ".doc"},
-      {"label": ".docx", "value": ".docx"},
-      {"label": ".xls", "value": ".xls"},
-      {"label": ".xlsx", "value": ".xlsx"},
-      {"label": ".ppt", "value": ".ppt"},
-      {"label": ".pptx", "value": ".pptx"},
-      {"label": ".txt", "value": ".txt"},
-      {"label": ".rtf", "value": ".rtf"},
-      {"label": ".csv", "value": ".csv"},
-      {"label": ".json", "value": ".json"},
-      {"label": ".xml", "value": ".xml"},
-      {"label": ".odt", "value": ".odt"},
-      {"label": ".epub", "value": ".epub"}
-    ]
-  },
-  {
-    "value": "图像",
-    "label": "图像",
-    "options": [
-      {"label": ".jpg", "value": ".jpg"},
-      {"label": ".jpeg", "value": ".jpeg"},
-      {"label": ".png", "value": ".png"},
-      {"label": ".gif", "value": ".gif"},
-      {"label": ".webp", "value": ".webp"},
-      {"label": ".bmp", "value": ".bmp"},
-      {"label": ".svg", "value": ".svg"},
-      {"label": ".psd", "value": ".psd"},
-      {"label": ".ai", "value": ".ai"},
-      {"label": ".eps", "value": ".eps"}
-    ]
-  },
-  {
-    "value": "音频",
-    "label": "音频",
-    "options": [
-      {"label": ".mp3", "value": ".mp3"},
-      {"label": ".wav", "value": ".wav"},
-      {"label": ".aac", "value": ".aac"},
-      {"label": ".ogg", "value": ".ogg"},
-      {"label": ".flac", "value": ".flac"},
-      {"label": ".midi", "value": ".midi"}
-    ]
-  },
-  {
-    "value": "视频",
-    "label": "视频",
-    "options": [
-      {"label": ".mp4", "value": ".mp4"},
-      {"label": ".mov", "value": ".mov"},
-      {"label": ".avi", "value": ".avi"},
-      {"label": ".mkv", "value": ".mkv"},
-      {"label": ".webm", "value": ".webm"},
-      {"label": ".flv", "value": ".flv"},
-      {"label": ".mpeg", "value": ".mpeg"}
-    ]
-  },
-  {
-    "value": "文件压缩",
-    "label": "文件压缩",
-    "options": [
-      {"label": ".zip", "value": ".zip"},
-      {"label": ".rar", "value": ".rar"},
-      {"label": ".7z", "value": ".7z"},
-      {"label": ".tar.gz", "value": ".tar.gz"}
-    ]
-  },
-  {
-    "value": "代码与编程",
-    "label": "代码与编程",
-    "options": [
-      {"label": ".html", "value": ".html"},
-      {"label": ".htm", "value": ".htm"},
-      {"label": ".css", "value": ".css"},
-      {"label": ".js", "value": ".js"},
-      {"label": ".py", "value": ".py"},
-      {"label": ".java", "value": ".java"},
-      {"label": ".class", "value": ".class"}
-    ]
-  },
-  {
-    "value": "可执行文件",
-    "label": "可执行文件",
-    "options": [
-      {"label": ".exe", "value": ".exe"},
-      {"label": ".apk", "value": ".apk"},
-      {"label": ".dmg", "value": ".dmg"},
-      {"label": ".sh", "value": ".sh"}
-    ]
-  }
-]
-
+const typeOptions = ref([])
 
 const onOk = async () => {
   const data = await formRef.value.validate()
@@ -190,19 +81,40 @@ const onOk = async () => {
 const onCancel = () => {
   formRef.value?.resetFields();
   formData.bodyType = props.value.bodyType;
-  formData.fileType = props.value.fileType;
+  formData.mediaType = props.value.mediaType;
   emit('cancel');
 }
 
+const queryList = () => {
+  getFileType().then((resp) => {
+    if(resp.success){
+      typeOptions.value = resp.result.map((item) => {
+        return {
+          value: item.text,
+          mediaType: item.mediaType,
+          options: item.suffix?.map((it) => {
+            return {
+              mediaType: item.mediaType,
+              value: it,
+            }
+          })
+        }
+      })
+    }
+  })
+}
+
 watch(
-    () => props.value,
-    () => {
-      formData.bodyType = props.value.bodyType;
-      formData.fileType = props.value.fileType;
+    () => visible.value,
+    (val) => {
+      if(val){
+        queryList();
+        formData.bodyType = props.value.bodyType;
+        formData.mediaType = props.value.mediaType;
+      }
     },
     {
-      immediate: true,
-      deep: true
+      immediate: true
     }
 );
 </script>
