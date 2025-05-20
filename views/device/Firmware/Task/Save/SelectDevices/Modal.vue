4ea3068f4951536f79e6fe7975bf2106b228df33
孙超 <54933585+sun-chaochao@users.noreply.github.com>
2025-05-20 11:21:27 +0800

    refactor: MSelect代码还原 (#83)
    * refactor: MSelect代码还原

---
+++ views/device/Firmware/Task/Save/SelectDevices/Modal.vue
@@ -0,0 +1,62 @@
+<template>
+  <a-modal
+      visible
+      :title="$t('Save.SelectDevices.386303-1')"
+      :ok-text="$t('Save.SelectDevices.386303-2')"
+      :cancel-text="$t('Save.SelectDevices.386303-3')"
+      :visible="true"
+      width="80%"
+      @cancel="handleCancel"
+      @ok="handleOk"
+  >
+    <j-card-select :showImage="false" v-model:value="activeKey" :options="options" :column="3" />
+
+    <div style="margin-top: 24px;">
+      <Org v-if="activeKey === 'org'" :data="data" :product-id="productId" />
+      <Table v-else :data="data" :product-id="productId" />
+    </div>
+  </a-modal>
+</template>
+<script setup name="SelectDevicesModal">
+import { useI18n } from 'vue-i18n';
+import Table from './Table.vue';
+import Org from './Org.vue';
+
+const { t: $t } = useI18n();
+const emit = defineEmits(['save', 'close']);
+
+const props = defineProps({
+  data: {
+    type: Object,
+    default: () => {},
+  },
+  productId: {
+    type: String,
+    default: '',
+  },
+});
+const activeKey = ref('')
+
+const options = [
+  {
+    label: '自定义',
+    value: 'self',
+  },
+  {
+    label: '全部',
+    value: 'all',
+  },
+  {
+    label: '按组织',
+    value: 'org',
+  }
+]
+
+const handleOk = () => {
+
+};
+
+const handleCancel = () => {
+
+};
+</script>
---
+++ views/device/Firmware/Task/Save/SelectDevices/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <a-input
+      :placeholder="$t('Save.SelectDevices.386303-0')"
+      v-model:value="checkAble"
+      :disabled="true"
+  >
+    <template #addonAfter>
+      <AIcon
+          :class="data.view ? 'disabled' : ''"
+          type="EditOutlined"
+          @click="onVisible"
+      />
+    </template>
+  </a-input>
+  <Modal v-if="visible" :data="data" :product-id="productId" />
+</template>
+<script lang="ts" setup name="SelectDevicesPage">
+import { useI18n } from 'vue-i18n';
+import Modal from './Modal.vue';
+
+const { t: $t } = useI18n();
+const emit = defineEmits(['update:modelValue', 'change']);
+
+const props = defineProps({
+  data: {
+    type: Object,
+    default: () => {},
+  },
+  productId: {
+    type: String,
+    default: '',
+  },
+});
+const visible = ref(false);
+const checkAble = ref();
+const onVisible = () => {
+  if(!props.data.view){
+    visible.value = true
+  }
+};
+
+</script>
+
+<style lang="less" scoped>
+.disabled {
+  pointer-events: auto !important;
+  cursor: not-allowed !important;
+}
+</style>
