3a3b28af93a6095872c26289c05ffe55eeea4a06
孙超 <54933585+sun-chaochao@users.noreply.github.com>
2025-11-28 17:33:11 +0800

    fix: 修改物模型的date的格式选择 (#151)
    * fix: 修改物模型的date的格式选择

--- views/link/plugin/Content.vue
+++ views/link/plugin/Content.vue
@@ -171,19 +171,19 @@ const columns = [
   {
     title: $t('plugin.index.293829-1'),
     dataIndex: 'id',
-    key: 'type',
+    key: 'id',
     ellipsis: true,
     search: {
-      type: 'input',
+      type: 'string',
     },
   },
   {
     title: $t('plugin.index.293829-3'),
     dataIndex: 'name',
-    key: 'type',
+    key: 'name',
     ellipsis: true,
     search: {
-      type: 'input',
+      type: 'string',
     },
   },
   {
