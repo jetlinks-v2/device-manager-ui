312b3a64ea8f3a6bf7453029be5477a27256d64e
孙超 <54933585+sun-chaochao@users.noreply.github.com>
2026-02-09 16:35:22 +0800

    fix: 存储策略为单列时，设备>>>物模型>>>继承产品的物模型可以同产品一致，可以修改指标值(#37961) (#175)
    * fix: 存储策略为单列时，设备>>>物模型>>>继承产品的物模型可以同产品一致，可以修改指标值(#37961)
    
    * fix: 设备运行状态查看事件，没有展示事件的值(#38589)
    
    * fix: 设备标签选择时间类型为date后，标签展示没有按照选择时间类型展示(#38661)

--- components/Metadata/components/Date/Item.vue
+++ components/Metadata/components/Date/Item.vue
@@ -39,7 +39,7 @@ const emit = defineEmits(["update:value"]);
 const options = [
   { label: "yyyy-MM-dd HH:mm:ss", value: "yyyy-MM-dd HH:mm:ss" },
   { label: "yyyy-MM-dd", value: "yyyy-MM-dd" },
-  { label: "hh:mm:ss", value: "hh:mm:ss" },
+  { label: "HH:mm:ss", value: "HH:mm:ss" },
 ];
 
 const _value = computed({
