ab2a0f09f4b9e71fef20cee6f6a709907759a179
孙超 <54933585+sun-chaochao@users.noreply.github.com>
2026-03-10 14:47:27 +0800

    fix: 修改数据解析的高度和设备关系重复的问题 (#180)
    * feat: 抛出设备列表查询接口
    
    * fix: 修改需判断是否有运维管理>>>资源库菜单（对应展示产品>>>快捷新增）
    
    * fix: 产品详情进行物模型搜索后，点击搜索结果，定位成功但是界面未跳转
    
    * fix: 当左侧组织数据过多时，进入“设备管理-设备-设备详情-设备关系”时，界面卡顿不能进行其他操作，进行优化(#37809)
    
    * fix: 修改混合模式,运行状态查询
    
    * fix: 修改数据解析的高度和设备关系重复的问题

--- views/device/Instance/Detail/index.vue
+++ views/device/Instance/Detail/index.vue
@@ -193,10 +193,10 @@ const initList = [
         key: 'Log',
         tab: $t('Detail.index.957187-15'),
     },
-    {
-        key: 'DeviceRelationship',
-        tab: '设备关系',
-    },
+    // {
+    //     key: 'DeviceRelationship',
+    //     tab: '设备关系',
+    // },
 ];
 
 const list = ref([...initList, ]);
