a654f1f0e01c145a4ec5456a5f6fc3dff38bd50f
xieqingsong <xieqingsong@cictci.com>
2026-03-30 19:03:15 +0800

    feat(网络模块):证书脱敏

--- api/link/certificate.ts
+++ api/link/certificate.ts
@@ -12,5 +12,11 @@ export const query = (data: object) => request.post(`/network/certificate/_query
 
 export const queryDetail = (id: string) => request.get(`/network/certificate/${id}`);
 
+export const querySecrecy = (data: object) => request.post(`/network/certificate/secrecy/__query`, data);
+
+export const querySecrecyDetail = (id: string) => request.get(`/network/certificate/secrecy/${id}`);
+
+export const updateSecrecy = (data: object) => request.post(`/network/certificate/secrecy/update`, data);
+
 export const remove = (id: string) => request.remove(`/network/certificate/${id}`);
 
